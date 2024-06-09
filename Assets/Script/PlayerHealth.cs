ce95e4aed138bd7950dc56fb7aef070af5f617a8
crizzby <joel.wekker@gmx.de>
2024-06-09 09:57:16 +0200

    Added changes

--- Assets/Script/PlayerHealth.cs
+++ Assets/Script/PlayerHealth.cs
@@ -1,20 +1,22 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 // This class handles the player's health and life count.
 public class PlayerHealth : MonoBehaviour
 {
     // The maximum number of lives the player can have.
     public int maxLives = 3;
-    
-
     // The current number of lives the player has.
     private int currentLives;
+    //References to the UI Text element that displays the health
+    public Text healthText;
 
     //Start is called before the first frame update
     void start()
     {
         //Initialize the currentLives to the maximum lives at the start of the game.
         currentLives = maxLives;
+        UpdateHealthUI();
     }
 
     //Method to apply damage to the player
@@ -23,12 +25,29 @@ public class PlayerHealth : MonoBehaviour
         //Reduce the current lives by the damage amount
         currentLives -= damageAmount;
 
-        //Log the current number of Lives to the console for debugging purposes.
-        Debug.Log("Player Lives: " + currentLives);
-
-        //Check if thr player's death scenario
-        Debug.Log("Player Died");
-
+        if (currentLives <= 0)
+        {
+            Debug.Log("Player Died");
+            GamerOver();
+        }
     
+    }
+
+    private void UpdateHealthUI()
+    {
+        healthText.text = "Health: " + currentLives;
+    }
+
+    private void GamerOver()
+    {
+        //Restart the current scene
+        UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex);
+    }
+
+    void update()
+    {
+        //Position the health text at the top left corner of the camera's viewpoint
+        Vector3 viewportPoint = Camera.main.ViewportToWorldPoint(new Vector3(0, 1, Camera.main.nearClipPlane));
+        healthText.transform.position = viewportPoint; 
     }
 }
